176085367475792aa80160fb72a5286e00c4f7d1
wadenya <alexwadenya001@gmail.com>
2023-09-25 15:42:37 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,10 +2,9 @@
 """Returns information about his/her TODO list progres"""
 
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
