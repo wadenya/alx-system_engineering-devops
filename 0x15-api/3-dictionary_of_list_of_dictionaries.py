ad717f18f565a27af32f55b4e2a0490242e78d78
wadenya <alexwadenya001@gmail.com>
2023-09-25 16:15:50 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""extend your Python script to export data in the JSON format"""
+""" extend your Python script to export data in the JSON format"""
+
 import json
 import requests
 
